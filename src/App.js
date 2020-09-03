86f94edb8fbc13582c5109058b55797b9fbbb4cb
MonteCristo <mjblee20@gmail.com>
2020-09-03 12:23:30 -0700

    mistake in making changes in master. Form component complete.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import HabitList from './components/habitList/HabitList';
+import Form from './components/habitList/form/Form';
 
 const App = function () {
   const [habits, setHabits] = useState(null);
 
-  const [newHabit, setNewHabit] = useState('');
-  const [oldHabit, setOldHabit] = useState('');
+  // const [newHabit, setNewHabit] = useState('');
+  // const [oldHabit, setOldHabit] = useState('');
+
   useEffect(() => {
     axios
       .get('/api/habits')
@@ -15,37 +17,38 @@ const App = function () {
       .catch((err) => console.log(err));
   }, []);
 
-  function submitForm() {
-    if (newHabit === '') {
-      alert('Please fill the username field');
-      return;
-    }
-    if (oldHabit === '') {
-      alert('Please fill the email field');
-      return;
-    }
-    axios
-      .post('/api/habits', {
-        newHabit: newHabit,
-        oldHabit: oldHabit,
-      })
-      .then(function () {
-        alert('Habit created successfully');
-        window.location.reload();
-      })
-      .catch(function () {
-        alert('Could not creat habit. Please try again');
-      });
-  }
+  // function submitForm() {
+  //   if (newHabit === '') {
+  //     alert('Please fill the username field');
+  //     return;
+  //   }
+  //   if (oldHabit === '') {
+  //     alert('Please fill the email field');
+  //     return;
+  //   }
+  //   axios
+  //     .post('/api/habits', {
+  //       newHabit: newHabit,
+  //       oldHabit: oldHabit,
+  //     })
+  //     .then(function () {
+  //       alert('Habit created successfully');
+  //       window.location.reload();
+  //     })
+  //     .catch(function () {
+  //       alert('Could not creat habit. Please try again');
+  //     });
+  // }
   return (
     <div>
       <HabitList habits={habits} />
 
-      <form onSubmit={submitForm}>
+      <Form />
+      {/* <form onSubmit={submitForm}>
         <input onChange={(e) => setNewHabit(e.target.value)} type='text' placeholder='Enter your new habit' />
         <input onChange={(e) => setOldHabit(e.target.value)} type='text' placeholder='Enter your old habit' />
         <input type='submit' />
-      </form>
+      </form> */}
     </div>
   );
 };
--- src/components/habitList/form/Form.js
+++ src/components/habitList/form/Form.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function form() {
-  return <div></div>;
+  const [newHabit, setNewHabit] = useState('');
+  const [oldHabit, setOldHabit] = useState('');
+
+  function submitForm() {
+    if (newHabit === '') {
+      alert('Please fill the username field');
+      return;
+    }
+    if (oldHabit === '') {
+      alert('Please fill the email field');
+      return;
+    }
+    axios
+      .post('/api/habits', {
+        newHabit: newHabit,
+        oldHabit: oldHabit,
+      })
+      .then(function () {
+        alert('Habit created successfully');
+        window.location.reload();
+      })
+      .catch(function () {
+        alert('Could not creat habit. Please try again');
+      });
+  }
+
+  return (
+    <form onSubmit={submitForm}>
+      <input onChange={(e) => setNewHabit(e.target.value)} type='text' placeholder='Enter your new habit' />
+      <input onChange={(e) => setOldHabit(e.target.value)} type='text' placeholder='Enter your old habit' />
+      <input type='submit' />
+    </form>
+  );
 }
 
 export default form;
